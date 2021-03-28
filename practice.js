dcce0bfd38df786ae398bde3e5b014fadfc9cf08
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-29 05:37:50 +0900

    practice.jsの追記

--- practice.js
+++ practice.js
@@ -1,32 +1,32 @@
 function LetterCountI(str) {
-  var maxCount = 0;
-  var word = '-1';
 
-  //split string into words based on spaces and count repeated characters
-  str.toLowerCase().split(" ").forEach(function(currentWord){
-      var hash = {};
+  // to LowerCase：呼び出す文字列を小文字に変換
+  str.toLowerCase();
 
-      //split word into characters and increment a hash map for repeated values
-      currentWord.split('').forEach(function(letter){
-          if (hash.hasOwnProperty(letter)) {
-              hash[letter]++;
-          } else {
-              hash[letter] = 1;
-          }
-      });
+  var arr = str.split(" ");
 
-      //covert the hash map to an array of character counts
-      var characterCounts = Object.keys(hash).map(function(key){ return hash[key]; });
+  var count = 0;
+  var word = "-1";
 
-      //find the maximum value in the squashed array
-      var currentMaxRepeatedCount = Math.max.apply(null, characterCounts);
-
-      //if the current word has a higher repeat count than previous max, replace it
-      if (currentMaxRepeatedCount > maxCount) {
-          maxCount = currentMaxRepeatedCount;
-          word = currentWord;
+  // for文「while文」、++x：加算
+  // i = 単語の数、つまりsplitした単語の数だけ以下の処理を実行する
+  for (var i = 0; i < arr.length; i++) {
+    // a = 単語の文字数、つまり単語内の文字の数だけ以下の処理を実行する
+    for (var a = 0; a < arr[i].length; a++) {
+      var countNew = 0; // カウントの再定義
+      // b = 重複した文字、つまり単語内の文字aに対して重複があるだけ以下の処理を実行する
+      for (var b = a + 1; b < arr[i].length; b++) {
+        // 重複を発見次第、countNewにカウント
+        if(arr[i][a] === arr[i][b])
+            countNew += 1;
       }
-  });
-
-  return word;
+      // 上記のfor文を繰り返し、countNewの値が0より大きい場合、countに定義。左記の処理を行った単語をwordに定義
+      if (countNew > count) {
+        count = countNew;
+        word = arr[i];
+      }
+      // wordの値を返す
+      return word;
+    }
+  }
 }
