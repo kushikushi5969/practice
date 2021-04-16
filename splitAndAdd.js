4c91b565c6d06857d5a98606c2f16834a5f0a1fc
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-16 23:52:56 +0900

    編集

--- splitAndAdd.js
+++ splitAndAdd.js
@@ -2,21 +2,60 @@ function splitAndAdd(str) {
   let string = String(str);
   let number = string.split('').map(Number);
   let result = 0;
+  let total = 0;
 
-  let total = number.reduce(function(sum, element){
+  let add = number.reduce(function(sum, element){
     return sum + element;
   }, 0);
-  if (String(total).length < 2) {
-    result = total;
-  } else {
-    let string = String(total)
+  result = add;
+  total = add;
+
+  console.log(String(total) + " =total2");
+  console.log(String(result) + " =result2");
+  for (var i = 1; i < String(total).length; i++) {
+    // if (String(total).length < 2) {
+    //   console.log(result + "if文");
+    // } else {
+    //   let string = String(result)
+    //   let number = string.split('').map(Number);
+    //   let total = number.reduce(function(sum, element){
+    //     return sum + element;
+    //   }, result);
+    //   result = total;
+    // }
+    let string = String(total);
     let number = string.split('').map(Number);
-    let totalsecond = number.reduce(function(sum, element){
+    let add = number.reduce(function(sum, element){
       return sum + element;
-    }, total);
-    result = totalsecond;
+    }, result);
+    total = add;
+    result = add;
+    console.log(String(total) + " =total2");
+    console.log(String(result) + " =result2");
   }
-  console.log(result);
+  console.log(String(result));
+
+  // for (var i = 1; i < String(str).length; i++) {
+  //   let string = String(str);
+  //   let number = string.split('').map(Number);
+  //   let total = number.reduce(function(sum, element){
+  //     return sum + element;
+  //   }, result);
+  //   result += total;
+  // }
+  // console.log(result);
+
+  // if (String(total).length < 2) {
+  //   console.log(result);
+  // } else {
+  //   let string = String(result)
+  //   let number = string.split('').map(Number);
+  //   let total = number.reduce(function(sum, element){
+  //     return sum + element;
+  //   }, result);
+  //   result = total;
+  // }
+  // console.log(String(result));
 }
 
 // 元のデータ
