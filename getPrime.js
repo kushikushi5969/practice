e02293a01b20b33a81c93d1eb4c7eb6941443e77
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-20 22:03:18 +0900

    getPrime.js作成

---
+++ getPrime.js
@@ -0,0 +1,29 @@
+function getPrime(number) {
+  let primes = [];
+  let result = 0;
+  let i,j;
+  if (number < 3) return 0;
+  let value = number - 1;
+
+  for (i = 2; i <= value; i++)
+    primes[i] = true;
+  for(i=2; i<=Math.sqrt(value); i++){
+    if(primes[i] === true){
+      for(j = i*2; j<=value; j+=i)
+        primes[j] = false;
+    }
+  }
+  for(i=2; i<=value; i++){
+    if(primes[i] === true){
+      result++;
+    }
+  }
+  return result
+}
+console.log("素数の数をカウント")
+console.log("2未満までの内、素数は" +getPrime(2) + "個")
+console.log("10未満までの内、素数は" +getPrime(10) + "個")
+console.log("36未満までの内、素数は" +getPrime(36) + "個")
+console.log("89未満までの内、素数は" +getPrime(89) + "個")
+console.log("100未満までの内、素数は" +getPrime(100) + "個")
+console.log("1000未満までの内、素数は" +getPrime(1000) + "個")
