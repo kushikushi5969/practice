fa14ad9541a6a21349287941e4400cc62ecec625
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-23 01:47:05 +0900

    コメントアウト

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -2,13 +2,13 @@ function longestConsecutive(scores) {
   let count = 0;
 
 
-  for (let i = 0; i < scores.lnegth; i++) {
-    let countNew = 0;
-    for (let a = i + 1; a < scores.length; a++) {
-      if (scores[index][i] === scores[index][a]) countNew += 1;
-      if (countNew > count) count = countNew;
-    }
-    return count
-  }
+  // for (let i = 0; i < scores.lnegth; i++) {
+  //   let countNew = 0;
+  //   for (let a = i + 1; a < scores.length; a++) {
+  //     if (scores[index][i] === scores[index][a]) countNew += 1;
+  //     if (countNew > count) count = countNew;
+  //   }
+  //   return count
+  // }
 }
 console.log(longestConsecutive([3,3,3,4,5]))
