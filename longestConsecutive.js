b5542732982e49a3511ea1c1f67bc8004483aeb2
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-23 01:46:21 +0900

    longestConsecutive.js作成

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -1,5 +1,14 @@
 function longestConsecutive(scores) {
   let count = 0;
 
-  
+
+  for (let i = 0; i < scores.lnegth; i++) {
+    let countNew = 0;
+    for (let a = i + 1; a < scores.length; a++) {
+      if (scores[index][i] === scores[index][a]) countNew += 1;
+      if (countNew > count) count = countNew;
+    }
+    return count
+  }
 }
+console.log(longestConsecutive([3,3,3,4,5]))
