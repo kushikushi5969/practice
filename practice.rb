97d2ca9ee1b7dc26a88cee6b089d9bfb9f44d1c0
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-25 00:50:51 +0900

    デフォルト追加

--- practice.rb
+++ practice.rb
@@ -18,3 +18,25 @@
 #   end
 # count -= 1
 # end
+
+data = gets.split(" ")
+many = data[0].to_i
+border = data[1].to_i
+id = 0;
+
+while 0 < many
+student = gets.split(" ")
+score = student[0].to_i
+absence = student[1].to_i
+id += 1
+  if border > 0
+    grades = score - absence * 5
+  else
+    grades = 0;
+  end
+
+  if border <= grades
+    puts id
+  end
+many -= 1
+end
