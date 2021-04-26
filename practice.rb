9b725569886c7b6704bf6148a09375e8c2e8017a
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-27 06:53:51 +0900

    adding

--- practice.rb
+++ practice.rb
@@ -40,3 +40,18 @@ id += 1
   end
 many -= 1
 end
+
+
+data = gets.split(" ")
+many = data[0].to_i
+border = data[1].to_i
+tree = gets.split(" ").map(&:to_i)
+count = gets.to_i
+
+while 0 < count
+  tree_ab = gets.split(" ").map(&:to_i)
+
+  tree.sum{ |x| x[tree_ab.map{ |a| a -= 1 }] }
+
+many -= 1
+end
