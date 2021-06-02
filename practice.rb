3ab24f586aac4476fb5b6fcfa8cfa1d6611e89a2
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-06-02 17:40:02 +0900

    デフォルト編集

--- practice.rb
+++ practice.rb
@@ -1,24 +1,6 @@
 # 練習用ファイル
 
-# paiza デフォルト
-# data = gets.split(" ")
-# criteria = data[0].to_i
-# count = data[1].to_i
-
-# while 0 < count
-# g = gets.to_i
-#   if g - criteria < 0
-#     puts criteria
-#   elsif g % criteria == 0
-#     puts g
-#   else
-#     result = g / criteria.to_f
-#     value = result.round
-#     puts criteria * value
-#   end
-# count -= 1
-# end
-
+# paizaデフォルト
 data = gets.split(" ")
 many = data[0].to_i
 border = data[1].to_i
